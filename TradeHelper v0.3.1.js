8b2b88909c3ad3b0bde8768547ed66ab1ef69ca6
lhoro <32704441+lhoro@users.noreply.github.com>
2020-04-14 12:57:37 +0200

    V 0.3.1

--- TradeHelper v0.3.1.js
+++ TradeHelper v0.3.1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @name         TH 0.3
-// @version      0.3
+// @name         TH 0.3.1
+// @version      0.3.1
 // @author       You
 // @match        https://steamcommunity.com/market/listings/730/*
 // @grant        none
@@ -22,33 +22,39 @@ let checkRequest = () => {
     let request = 0;
     request = $('#market_commodity_buyrequests').text();
     request = request.slice(25, 200);
-    request = request.match(/[0-9]*\,[0-9][0-9]/);
-    request = parseFloat(request[0].toString().replace(",", "."));
+    request = request.match(/(\d\s)?[0-9]*\,[0-9][0-9]/);
+    request = request[0].toString().replace(" ", "")
+    request = parseFloat(request.replace(",", "."));
     checkPrices(request);
 }
 
 // Check Market Prices
 let checkPrices = (request) => {
-    let i=0;
+    let i=0, price, profit;
     let profits = [];
-    for(i;i<5;i++){
+    let results =  $('.market_listing_price_without_fee').length;
+
+    while(i < 5 && i < results ){
         let price = $('.market_listing_price_without_fee').eq(i).text();
-        price = price.match(/[0-9]*\,[0-9][0-9]/);
+        price = price.match(/(\d\s)?[0-9]*\,[0-9][0-9]/);
+        price = price.toString().replace(" ", "")
         price = parseFloat(price.toString().replace(",", "."));
         let profit = price - request;
         profit = profit.toFixed(2);
         profits.push(parseFloat(profit));
+        i++;
     }
-    show(profits);
+
+    show(profits , i);
 };
 
 // Show informations
-let show = (profits) => {
+let show = (profits, resoults) => {
     let i = 0;
     let color = 0;
     let text = "";
 
-    for(i;i<5;i++){
+    for(i;i<resoults ;i++){
         if(profits[i] > 0)
             color = "green";
         else
@@ -56,8 +62,8 @@ let show = (profits) => {
         text = text + "<span style='color:"+color+"; margin:10px'>"+profits[i]+"</span>";
     }
 
-    $('<div/>',{
-        html: text,
-        style: "background-color:black; color:white; font-size:20px; position: fixed; top:104px; text-align:center; width: 50%; left:50%; transform:translateX(-50%);"
-    }).appendTo('body');
+   $('<div/>',{
+       html: text,
+       style: "background-color:black; color:white; font-size:20px; position: fixed; top:104px; text-align:center; width: 50%; left:50%; transform:translateX(-50%);"
+   }).appendTo('body');
 }
